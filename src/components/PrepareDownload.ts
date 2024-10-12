bac6e856f7076225b2126f01e3adada4f17de915
d0uble-happiness <double.happiness.records@gmail.com>
2024-10-12 14:16:01 +0100

    fix: used encodeURIComponent instead of encodeURI as it encodes certain characters but leaves the # symbol unchanged, causing potential conflicts

--- src/components/PrepareDownload.ts
+++ src/components/PrepareDownload.ts
@@ -8,12 +8,9 @@ export default {
 }
 
 export function prepareDownload(data: any[]) {
-  const csvContent =
-    'data:text/csv;charset=utf-8,' +
-    ROW_NAMES.join(',') +
-    '\n' +
-    data.map((e) => e.join(',')).join('\n')
-  const encodedUri = encodeURI(csvContent)
+  const csvRows = [ROW_NAMES.join(','), ...data.map((e) => e.join(','))].join('\n')
+
+  const encodedUri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvRows)
   const link = document.createElement('a')
   link.setAttribute('href', encodedUri)
   link.setAttribute('download', 'my_data.csv')
